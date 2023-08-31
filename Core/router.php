f0a28bb85d0da938a3994cd5d20f730a7c1874ba
JReed <jason.reed@fayette.kyschools.us>
2023-08-30 21:20:12 -0400

    Ep34: New Controller

--- Core/router.php
+++ Core/router.php
@@ -1,5 +1,59 @@
 <?php
 
+namespace Core;
+
+class Router{
+    protected $routes = [];
+
+    public function add($method, $uri, $controller){
+        $this->routes[]=[
+            'uri' => $uri,
+            'controller' => $controller,
+            'method' => $method
+        ];
+    }
+
+    public function get( $uri, $controller ){
+        //push a new item onto the routes array
+        $this->add("GET", $uri, $controller);
+    }
+    public function post($uri, $controller){
+        $this->add("POST", $uri, $controller);
+    }
+    public function delete($uri, $controller){
+        $this->add("DELETE", $uri, $controller);
+    }
+    public function patch($uri, $controller){
+        $this->add("PATCH", $uri, $controller);
+    }
+    public function put($uri, $controller){
+        $this->add("PUT", $uri, $controller);
+    }
+
+    public function route($uri, $method){
+        //loop through our routes array
+        foreach( $this->routes as $route){
+            //if the uri AND method of the array match what we received
+            if( $route['uri'] == $uri && $route['method'] == strtoupper($method))
+                return require base_path($route['controller']);
+        }//end for
+
+        //never found a match.  Abort
+        $this -> abort();
+
+    }
+
+    protected function abort($code = 404) {
+        http_response_code($code);
+    
+        require base_path("views/{$code}.php");
+    
+        die();
+    }
+
+}//end class
+
+/*
  function routeToController($uri, $routes) {
     if (array_key_exists($uri, $routes)) {
         require base_path($routes[$uri]);
@@ -8,15 +62,10 @@
     }
 }
 
-function abort($code = 404) {
-    http_response_code($code);
 
-    require base_path("views/{$code}.php");
-
-    die();
-}
 
 $routes = require base_path('routes.php');
 $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 
 routeToController($uri, $routes);
+*/
--- public/index.php
+++ public/index.php
@@ -11,6 +11,19 @@ spl_autoload_register(function ($class) {
     require base_path("{$class}.php");
 });
 
-require base_path('Core/router.php');
+$router = new \Core\Router();
+
+$routes = require base_path('routes.php');
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
+
+//IF there is a _method field in the post request, use IT
+//otherwise use the method (get/post) that you received from the server
+$method = $_POST['_method'] ?? $_SERVER['REQUEST_METHOD'];
+
+
+$router->route($uri, $method);
+//routeToController($uri, $routes);
+
+//require base_path('Core/router.php');
 
 
