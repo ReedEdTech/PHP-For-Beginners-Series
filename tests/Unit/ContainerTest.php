871dfce49d147e20a6b7f5c9e7dfa18b3c6a8180
JReed <jason.reed@fayette.kyschools.us>
2023-09-04 08:37:55 -0400

    Ep 48 Install Packages

---
+++ tests/Unit/ContainerTest.php
@@ -0,0 +1,13 @@
+<?php
+
+use Core\Container;
+
+test('it can resolve something out of the container', function () {
+    //arrange the world 
+    $container = new Container();
+    $container->bind('foo', fn()=>'bar');
+    //act
+    $result = $container->resolve('foo');
+    //assert or expect
+    expect($result)->toEqual('bar');
+});
